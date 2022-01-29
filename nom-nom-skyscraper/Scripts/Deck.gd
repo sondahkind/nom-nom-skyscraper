aebf1f9f5bf9a50b839a38ccfed76bd3d48cafd4
sanderol <sanderol@uni-bremen.de>
2022-01-29 02:03:44 +0100

    added cards to deck

--- nom-nom-skyscraper/Scripts/Deck.gd
+++ nom-nom-skyscraper/Scripts/Deck.gd
@@ -2,30 +2,33 @@ extends Object
 
 class Deck:
 
-    var deck = [] 
+	var deck = [] 
 
-    # TODO add Deck stuff
+	# TODO add Deck stuff
 
-    # Is the constructor is calles with Deck.new() an creates new object
-    func _init(deck_size):
-        self.deck_size = deck_size
-        pass
+	# Is the constructor is calles with Deck.new() an creates new object
+	func _init():
+		pass
 
-    func add_cards(card, ammount):
-        for x in ammount:
-            deck.append(card)
+	func add_cards(card, ammount):
+		for x in ammount:
+			deck.append(card)
 
 
-    func shuffle():
-        var shuffled_deck = []
-        while deck.size() > 0:
-            var rng = RandomNumberGenerator.new()
-            rng.randomize()
-            var card_number = rng.randi_range(0, deck.size())
-            shuffled_deck.append(deck[card_number])
-            deck.remove(card_number)
-        deck = shuffled_deck
+	func shuffle():
+		var shuffled_deck = []
+        #Todo
+		while deck.size() > 0:
+			var rng = RandomNumberGenerator.new()
+			rng.randomize()
+			var card_number = rng.randi_range(0, deck.size())
+			shuffled_deck.append(deck[card_number])
+			deck.remove(card_number)
+		deck = shuffled_deck
 
-    func draw_top_card():
-        return deck.pop_front() 
+	func draw_top_card():
+		if (deck.size() > 0):
+			return deck.pop_front() 
+		else:
+			return null
 
